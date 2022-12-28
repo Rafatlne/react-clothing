07ec22b4d5f1268608703f26ed764e9b3c61037a
Ashik Ahamed Aman Rafat <rafat@Ashiks-MacBook-Air.local>
2022-12-28 22:51:19 +0600

    Added: Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Directory from "./components/directory/directory.component" 
+import { Routes, Route } from 'react-router-dom';
+
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+
+const Shop = () => {
+  return <h1>I am a shop page</h1>
+}
 
 const App = () => {
-  return <Directory />
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 };
 
 export default App;
